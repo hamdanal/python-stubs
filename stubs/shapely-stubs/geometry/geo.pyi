de7f18598113aa18a0fdb54348b7d8b6b589fb69
Ali Hamdan <ali.hamdan.dev@gmail.com>
2024-06-01 10:41:50 +0200

    Remove shapely stubs (#7)

--- stubs/shapely-stubs/geometry/geo.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from shapely._typing import SupportsGeoInterface
-from shapely.geometry.base import BaseGeometry
-from shapely.geometry.polygon import Polygon
-
-def box(minx: float, miny: float, maxx: float, maxy: float, ccw: bool = True) -> Polygon: ...
-def shape(context: dict[str, Any] | SupportsGeoInterface) -> BaseGeometry: ...
-def mapping(ob: SupportsGeoInterface) -> dict[str, Any]: ...
