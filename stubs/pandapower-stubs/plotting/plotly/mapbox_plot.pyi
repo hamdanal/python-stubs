604206a0f6637523f24227983e54578f786e8b44
Ali Hamdan <ali.hamdan.dev@gmail.com>
2025-06-22 11:32:16 +0200

    Fix

--- stubs/pandapower-stubs/plotting/plotly/mapbox_plot.pyi
+++ stubs/pandapower-stubs/plotting/plotly/mapbox_plot.pyi
@@ -1,7 +1,1 @@
-from typing_extensions import deprecated
-
-from pandapower.auxiliary import pandapowerNet
-
-@deprecated("Use pandapower.geo.convert_crs instead")
-def geo_data_to_latlong(net: pandapowerNet, projection: str) -> None: ...
 def set_mapbox_token(token: str) -> None: ...
